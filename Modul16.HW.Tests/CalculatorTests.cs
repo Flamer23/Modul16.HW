f865547a8dd055659660ab654e10a73bed9d8129
Илья Зуев <zueviliya777@gmail.com>
2024-04-05 00:30:22 +0300

    Добавьте файлы проекта.

---
+++ Modul16.HW.Tests/CalculatorTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace Modul16.HW.Tests
+{
+    [TestFixture]
+    public class CalculatorTets
+    {
+        [Test]
+        public void Subtraction_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Subtraction(300, 10) == 290);
+        }
+
+        [Test]
+        public void Division_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Division(200, 10) == 20);
+        }
+
+        [Test]
+        public void Division_MustThrowException()
+        {
+            var calculator = new Calculator();
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(30, 0));
+        }
+
+        [Test]
+        public void Additional_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Additional(300, 10) == 310);
+        }
+
+        [Test]
+        public void Miltiplication_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(300, 10) == 3000);
+        }
+    }
+}
